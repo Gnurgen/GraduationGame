eb98e2914c533404beca471a87d50ee1ce4f5e8e
Kristoffer Lind Holm <klh12@student.aau.dk>
2016-11-03 09:38:45 +0100

    no message

--- Assets/angle.cs
+++ Assets/angle.cs
@@ -12,9 +12,10 @@ public class angle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        print(Vector3.Angle(transform.position, lol.transform.position));
-        angles = Vector3.Angle(transform.position, lol.transform.position);
-        s1.transform.position =  Quaternion.AngleAxis(angles, Vector3.up) * -transform.forward + transform.position;
-        s2.transform.position = Quaternion.AngleAxis(angles, Vector3.up) * transform.forward + transform.position;
+      
+        angles = Mathf.Atan2(transform.position.z - lol.transform.position.z, lol.transform.position.x - transform.position.x) * 180 / Mathf.PI;
+        print(angles);
+        s1.transform.position =  Quaternion.AngleAxis(angles, Vector3.up) * -Vector3.forward + transform.position;
+        s2.transform.position = Quaternion.AngleAxis(angles, Vector3.up) * Vector3.forward + transform.position;
     }
 }
