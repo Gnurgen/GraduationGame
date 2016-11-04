ce6c4ca72b357d88795657a36980e56639f41e7e
Kristoffer Lind Holm <klh12@student.aau.dk>
2016-11-04 09:32:48 +0100

    Rework lol

---
+++ Assets/Scripts/CurveDraw.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CurveDraw : MonoBehaviour {
+
+	//public Vector3[] points;
+
+	public GameObject line;
+	private bool drawing;
+	private List<Vector3> curvePoints;
+	public int verticeIndex;
+	public int indicieIndex;
+	private Mesh mesh;
+	private Vector3[] vertices;
+	private int[] indicies;
+	private float angle;
+    TestMoveAtk TMA;
+    // Use this for initialization
+    void Start () {
+		drawing = false;
+		curvePoints = new List<Vector3> ();
+		mesh = new Mesh ();
+		vertices = new Vector3[500];
+		indicies = new int[1500];
+		verticeIndex = 0;
+		indicieIndex = 0;
+        TMA = FindObjectOfType<TestMoveAtk>();
+        TMA.OnDrag += AddPoint;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void StartDraw(){
+		drawing = true;
+		curvePoints = new List<Vector3> (); 
+	}
+
+	public void AddPoint(Vector3 p){
+		if (curvePoints.Count == 0) {
+			InitializeMesh (p);
+		} else {
+			Debug.Log ("Added point");
+			DrawNewPoint (p);
+		}
+		curvePoints.Add (p);
+	}
+
+	Vector3 CastPoint(Vector2 p){
+		return Camera.main.ScreenToWorldPoint (new Vector3 (p.x, p.y, 0));
+	}
+
+	Vector3 CastPoint(Vector3 p){
+		return Camera.main.ScreenToWorldPoint (p);
+	}
+
+	void InitializeMesh(Vector3 p){
+        p = new Vector3(p.x, 0, p.z);
+		vertices[verticeIndex] = p;
+		verticeIndex++;
+		vertices[verticeIndex] = p;
+		verticeIndex++;
+		vertices[verticeIndex] = p;
+		verticeIndex++;
+		vertices[verticeIndex] = p;
+		verticeIndex++;
+		mesh.vertices = vertices;
+
+		indicies[indicieIndex] = 0;
+		indicieIndex++;
+		indicies[indicieIndex] = 2;
+		indicieIndex++;
+		indicies[indicieIndex] = 1;
+		indicieIndex++;
+		indicies[indicieIndex] = 2;
+		indicieIndex++;
+		indicies[indicieIndex] = 3;
+		indicieIndex++;
+		indicies[indicieIndex] = 1;
+		indicieIndex++;
+		mesh.triangles = indicies;
+		line.GetComponent<MeshFilter> ().mesh = mesh;
+	}
+
+	void DrawNewPoint(Vector3 newPoint){
+        newPoint = new Vector3(newPoint.x, 0.1f, newPoint.z);
+        CheckArraySizes ();
+		Vector3 previousPoint = curvePoints [curvePoints.Count - 1];
+		angle = Mathf.Atan2(newPoint.z - previousPoint.z, previousPoint.x - newPoint.x) * 180 / Mathf.PI;
+		vertices[verticeIndex] = Quaternion.AngleAxis(angle, Vector3.up) * -Vector3.forward + newPoint;
+		verticeIndex++;
+		vertices[verticeIndex] = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward + newPoint;
+		verticeIndex++;
+
+		indicies [indicieIndex] = verticeIndex - 2;
+		indicieIndex++;
+		indicies [indicieIndex] = verticeIndex - 3;
+		indicieIndex++;
+		indicies [indicieIndex] = verticeIndex - 4;
+		indicieIndex++;
+		indicies [indicieIndex] = verticeIndex - 2;
+		indicieIndex++;
+		indicies [indicieIndex] = verticeIndex - 1;
+		indicieIndex++;
+		indicies [indicieIndex] = verticeIndex - 3;
+		indicieIndex++;
+		mesh.vertices = vertices;
+		mesh.triangles = indicies;
+		line.GetComponent<MeshFilter> ().mesh = mesh;
+	}
+
+	void CheckArraySizes(){
+		if (verticeIndex >= vertices.Length - 2) {
+			Vector3[] temp = new Vector3[vertices.Length + 100];
+			for (int i = 0; i < verticeIndex; i++) {
+				temp [i] = vertices [i];
+			}
+			vertices = temp;
+		}
+		if (indicieIndex >= indicies.Length - 2) {
+			int[] temp = new int[indicies.Length + 300];
+			for (int i = 0; i < indicieIndex; i++) {
+				temp [i] = indicies [i];
+			}
+			indicies = temp;
+		}
+	}
+
+	public Vector3[] GetPoints(){
+		return vertices;
+	}
+		
+}
---
+++ Assets/TestMoveAtk.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class TestMoveAtk : MonoBehaviour
+{
+
+    public float shootDistance = 10f;
+    public float shootRate = .5f;
+    // public PlayerShooting shootingScript;
+
+      private Animator anim;
+    private NavMeshAgent navMeshAgent;
+    private Transform targetedEnemy;
+    private Ray shootRay;
+    private RaycastHit shootHit;
+    private bool walking;
+    private bool enemyClicked;
+    private float nextFire;
+
+
+    public enum InputState { draw, move, menu };
+    public InputState curState;
+    [SerializeField]
+    private float _minSwipeSpeed, _minSwipeDist, _doubleTapTime, _minDoubleTapDist, _distPointResolution;
+    private Vector2 origPos, endPos, curPos;
+    private float _dragDist, _dragSpeed, _dTapTimer;
+    private int h, w;
+    private bool _dTap = false, _isSwipe = false;
+    Camera myCam;
+
+    public delegate void DragDelegate(Vector3 p);
+    public event DragDelegate OnDrag;
+
+    void Start()
+    {
+        myCam = FindObjectOfType<Camera>();
+        h = Screen.height;
+        w = Screen.width;
+    }
+
+    private Vector2 normPos(Vector2 vec)
+    {
+        vec = new Vector2(vec.x / w, vec.y / h);
+        return vec;
+    }
+
+    private void PostNextDragPoint()
+    {
+        print("PostNextDragPoint");
+        _dragDist = 0;
+        Vector3 inputPos = curPos;
+        inputPos.z = 1f;
+        Vector3 worldPos = myCam.ScreenToWorldPoint(inputPos);
+        if (OnDrag != null)
+            OnDrag(worldPos);
+
+    }
+
+    private void PostDoubleTapEvent()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(endPos);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        navMeshAgent.Warp(hit.point);
+        navMeshAgent.Resume();
+
+        print("PostDoubleTapEvent");
+    }
+
+    private void PostSwipeEvent()
+    {
+        print("PostSwipeEvent");
+
+        Ray ray = Camera.main.ScreenPointToRay(endPos);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        navMeshAgent.destination = hit.point;
+        navMeshAgent.destination = transform.position;
+        transform.rotation = Quaternion.FromToRotation(transform.position, hit.point);
+        anim.SetTrigger("Attack");
+        _dragDist = 0;
+        _isSwipe = false;
+    }
+
+    private void PostTapEvent()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(endPos);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        navMeshAgent.destination = hit.point;
+        navMeshAgent.Resume();
+
+        print("PostTapEvent");
+    }
+
+// Use this for initialization
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       if(Input.touchCount > 4)
+        {
+            print("5");
+            curState = 0;
+        }
+
+        if (curState != InputState.menu && Input.touchCount > 0)
+    {
+        if (Input.touches[0].phase == TouchPhase.Began)
+        {
+            origPos = Input.touches[0].position;
+            curPos = origPos;
+        }
+        if (Input.touches[0].phase == TouchPhase.Ended)
+        {
+            if (curState == InputState.move)
+            {
+                if (_dTapTimer <= _doubleTapTime && Vector2.Distance(normPos(origPos), normPos(endPos)) <= _minDoubleTapDist)
+                    PostDoubleTapEvent();
+            }
+            endPos = Input.touches[0].position;
+            if (!_isSwipe)
+                PostTapEvent();
+            else if (_isSwipe)
+                PostSwipeEvent();
+            _dTapTimer = 0;
+        }
+        if (Input.touches[0].phase == TouchPhase.Moved)
+        {
+            _dragSpeed = normPos(Input.touches[0].deltaPosition).magnitude / Time.deltaTime;
+            _dragDist += _dragSpeed * Time.deltaTime;
+            curPos = Input.touches[0].position;
+            if (_dragDist >= _distPointResolution && curState == InputState.draw)
+                PostNextDragPoint();
+            else if (curState == InputState.move && _dragSpeed >= _minSwipeSpeed && _dragDist >= _minSwipeDist)
+                _isSwipe = true;
+        }
+    }
+    if (_dTapTimer <= _doubleTapTime)
+        _dTapTimer += Time.deltaTime;
+                 
+    }
+}
+
