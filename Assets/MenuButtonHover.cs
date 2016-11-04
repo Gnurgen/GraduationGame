36fbf1fc3b0edf6ac93563d6b5de70c60b6546ba
Mette <mette9jakobsen@homail.com>
2016-11-04 13:55:39 +0100

    Menu wheel can select and stuff!

---
+++ Assets/MenuButtonHover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuButtonHover : MonoBehaviour {
+    public GameObject normal, hover;
+  
+	void Start () {
+	}
+    public void hoverImageOn() {
+        hover.SetActive(true);
+    }
+    public void hoverImageOff()
+    {
+        hover.SetActive(false);
+    }
+}
