89b38413a1a89fc95781a4066ab1fb0ce2ecd331
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-11-04 12:25:26 +0100

    Snap fix

---
+++ Assets/Editor/SnapToSurface.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SnapToSurface : Editor{
+
+    [MenuItem("Tools/Snap to Surface %w", false,0)]
+    public static void Snap()
+    {
+        Object[] objects = Selection.objects;
+
+
+    }
+}
