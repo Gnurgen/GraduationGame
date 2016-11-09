8c6961f08af6800ba4e633e0bc5013c3798e96f3
Mette <mette9jakobsen@homail.com>
2016-11-09 15:13:36 +0100

    Level up GUI unfinished

--- Assets/Scripts/LevelUpGUI.cs
+++ Assets/Scripts/LevelUpGUI.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class LevelUpGUI : MonoBehaviour
 {
     //TEst stuff
-    public GameObject testMouse;
-    private Vector3 mousePos;
+   // public GameObject testMouse;
+   // private Vector3 mousePos;
     public bool clicked = false;
     public bool released = false;
     private bool isCard = false;
@@ -21,8 +21,17 @@ public class LevelUpGUI : MonoBehaviour
     private Vector3 selectedCardPos;
     private Vector3 tempPos;
 
+    //Manager
+    private InputManager IM;
+    int ID;
+
     void Start()
     {
+        IM = FindObjectOfType<InputManager>();
+        IM.OnFirstTouchBeginSub(click, ID);
+        IM.OnFirstTouchEndSub(release, ID);
+        IM.OnFirstTouchMoveSub(followTouch, ID);
+
         cardColliders = new Collider[3];
         slotColliders = new Collider[3];
 
@@ -36,28 +45,22 @@ public class LevelUpGUI : MonoBehaviour
         }
     }
 
-    void Update()
+    void click(Vector2 touchPos)
     {
-        mousePos = testMouse.transform.position;
-        if (clicked == true)
-            selectedCard.transform.position = mousePos;
-        if (clicked == true && released == true)
-            release();
-    }
-
-    public void click()
-    {
+        Vector3 temp = new Vector3(touchPos.x - Screen.width / 2, touchPos.y - Screen.height / 2 , 0);
+        Debug.Log(touchPos + " and " + cardColliders[1].bounds);
         for (int i = 0; i < 3; i++)
         {
-            if (cardColliders[i].bounds.Contains(mousePos))
+            if (cardColliders[i].bounds.Contains(temp))
             {
+                Debug.Log("Click Card");
                 selectedCard = cards[i];
                 selectedCardPos = selectedCard.transform.position;
                 selectedCardNum = i;
                 isCard = true;
                 clicked = true;
             }
-            if (slotColliders[i].bounds.Contains(mousePos))
+            if (slotColliders[i].bounds.Contains(temp))
             {
                // Debug.Log("Card " + i + " selected");
                 selectedCard = slots[i];
@@ -68,17 +71,17 @@ public class LevelUpGUI : MonoBehaviour
             }
         }
     }
-    void release()
+    void release(Vector2 touchPos)
     {
         clicked = false;
         released = false;
         for (int i = 0; i < 3; i++) 
         {
-            if (slotColliders[i].bounds.Contains(mousePos)&& isCard == true)
+            if (slotColliders[i].bounds.Contains(touchPos) && isCard == true)
             {
                 skillSelected(selectedCardNum ,i);
             }
-            else if (slotColliders[i].bounds.Contains(mousePos) && isCard == false && i != selectedCardNum)
+            else if (slotColliders[i].bounds.Contains(touchPos) && isCard == false && i != selectedCardNum)
             {
                 tempPos = slots[i].transform.position;
                 //Debug.Log("Card " + i + " pending switch");
@@ -99,7 +102,10 @@ public class LevelUpGUI : MonoBehaviour
        // Debug.Log("Card #" + cardNum + " Has been moved to slot #" + slotNum);
         levelUpGUI.SetActive(false);
     }
-    public void testRelease() {
-        released = true;
+    void followTouch(Vector2 touchPos) {        
+        selectedCard.transform.position = touchPos;
+        Debug.Log("Draggin");
     }
+
+
 }
