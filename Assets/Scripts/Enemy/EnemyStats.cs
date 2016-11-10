0bfba48acc1754885cfb52d237c407d14c5d1fad
Nicolai Skovhus <nskovh11@student.aau.dk>
2016-11-10 10:39:41 +0100

    no message

---
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStats : Health {
+    [SerializeField]
+    private float _baseDamage;
+
+    private enum Setting {_1x, _2x, _3x,_4x, _5x}
+    [SerializeField]
+    private Setting strength;
+    void Awake()
+    {
+       setHealthVars(strength.GetHashCode()+1);
+    }
+
+    void Update()
+    {
+    }
+}
