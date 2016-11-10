f114899d9237113291a90c8127478c4f13386c23
Nicolai Skovhus <nskovh11@student.aau.dk>
2016-11-10 14:01:35 +0100

    no message

---
+++ Assets/Scripts/Enemy/EnemyShieldAttack.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShieldAttack : MonoBehaviour
+{
+
+    private Transform body;
+    private int dmg;
+    private Collider myCol;
+    void Start()
+    {
+        myCol = GetComponent<Collider>();
+        myCol.isTrigger = true;
+        myCol.enabled = false;
+        body = transform.parent;
+        setMyDmg();
+    }
+
+    void setMyDmg()
+    {
+        if (body.tag == "MeleeShield")
+        {
+            dmg = body.GetComponent<EnemyStats>().damage;
+            body.SendMessage("RegisterWeapon");
+        }
+        else
+        {
+            body = body.parent;
+            setMyDmg();
+        }
+    }
+
+    public void Swing(bool swing)
+    {
+        myCol.enabled = swing;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            col.GetComponent<Health>().decreaseHealth(dmg);
+            myCol.enabled = false;
+            GameManager.events.EnemyAttackHit(body.gameObject, dmg);
+        }
+    }
+}
