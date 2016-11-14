f4b95907d7d61daad72359788a55b5a5097c8a1a
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-11-14 10:21:08 +0100

    Tool for updating room objects added

--- Assets/Editor/RoomBuilderEditor.cs
+++ Assets/Editor/RoomBuilderEditor.cs
@@ -64,15 +64,89 @@ public class RoomEditor : Editor {
         }
     }
 
-    [MenuItem("Tools/Room Builder/Update References", false, 22)]
-    public static void UpdateRoomReferences()
+    [MenuItem("Tools/Room Builder/Update Room Resources/Walls", false, 22)]
+    public static void UpdateWallReferences()
     {
-        List<GameObject> objectList = Resources.LoadAll("Room").Cast<GameObject>().ToList();
-        // For each [decoration|enemy|destructible] 
-        // if reference == prefab
-        // replace object with prefab
-        // end for
-        // overwrite room
+        int i;
+        int j;
+        int l;
+        int count = 0;
+        RoomWall[] roomWalls;
+        GameObject room;
+        List<GameObject> rooms = Resources.LoadAll("Room").Cast<GameObject>().ToList();
+        List<GameObject> walls = Resources.LoadAll("Wall").Cast<GameObject>().ToList();
+
+        for (i = 0; i < rooms.Count; i++)
+        {
+            room = PrefabUtility.InstantiatePrefab(rooms[i]) as GameObject;
+            roomWalls = room.GetComponentsInChildren<RoomWall>();
+
+            for (j = 0; j < roomWalls.Length; j++)
+            {
+                for (l = 0; l < walls.Count; l++)
+                {
+                    if (roomWalls[j].referenceName == walls[l].GetComponent<RoomWall>().referenceName)
+                    {
+                        replace(roomWalls[j].gameObject, walls[l]);
+                        count++;
+                    }
+                }
+            }
+            PrefabUtility.ReplacePrefab(room, rooms[i], ReplacePrefabOptions.ConnectToPrefab | ReplacePrefabOptions.ReplaceNameBased);
+            DestroyImmediate(room);
+        }
+
+        Debug.Log("Updated " + count + " objects");
+    }
+
+    [MenuItem("Tools/Room Builder/Update Room Resources/Tiles", false, 23)]
+    public static void UpdateTileReferences()
+    {
+        int i;
+        int j;
+        int l;
+        int count = 0;
+        RoomTile[] roomTiles;
+        GameObject room;
+        List<GameObject> rooms = Resources.LoadAll("Room").Cast<GameObject>().ToList();
+        List<GameObject> tiles = Resources.LoadAll("Tile").Cast<GameObject>().ToList();
+
+        for (i = 0; i < rooms.Count; i++)
+        {
+            room = PrefabUtility.InstantiatePrefab(rooms[i]) as GameObject;
+            roomTiles = room.GetComponentsInChildren<RoomTile>();
+
+            for (j = 0; j < roomTiles.Length; j++)
+            {
+                for (l = 0; l < tiles.Count; l++)
+                {
+                    if (roomTiles[j].referenceName == tiles[l].GetComponent<RoomTile>().referenceName)
+                    {
+                        replace(roomTiles[j].gameObject, tiles[l]);
+                        count++;
+                    }
+                }
+            }
+            PrefabUtility.ReplacePrefab(room, rooms[i], ReplacePrefabOptions.ConnectToPrefab | ReplacePrefabOptions.ReplaceNameBased);
+            DestroyImmediate(room);
+        }
+
+        Debug.Log("Updated " + count + " objects");
+    }
+    
+
+    private static void replace(GameObject original, GameObject replacement)
+    {
+        GameObject obj = Instantiate(replacement) as GameObject;
+        obj.name = original.name;
+        Undo.RegisterCreatedObjectUndo(obj, "Created replacement object");
+        obj.transform.position = original.transform.position;
+        obj.transform.rotation = original.transform.rotation;
+        obj.transform.localScale = original.transform.localScale;
+
+        obj.transform.parent = original.transform.parent;
+        obj.transform.SetSiblingIndex(original.transform.GetSiblingIndex());
+        Undo.DestroyObjectImmediate(original);
     }
 
     override public void OnInspectorGUI()
