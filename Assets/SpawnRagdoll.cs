00df030082ed34d3fd561a3353648922a0c5dea4
Nicolai Skovhus <nskovh11@student.aau.dk>
2016-11-21 11:06:47 +0100

    Ragdolls

--- Assets/SpawnRagdoll.cs
+++ Assets/SpawnRagdoll.cs
@@ -5,6 +5,9 @@ public class SpawnRagdoll : MonoBehaviour {
     public Transform[] Ragdoll;
     PoolManager poolManager;
     private string _tag;
+
+    [SerializeField]
+    bool debug = false;
     public string myTag
     {
         set
@@ -16,11 +19,21 @@ public class SpawnRagdoll : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         poolManager = FindObjectOfType<PoolManager>();
+        if(gameObject.tag=="Player")
+            _tag = "KumoRagdoll";
     }
 
     public void Execute()
     {
         poolManager.GenerateRagdoll(Ragdoll,_tag);
         Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (debug)
+        {
+            Execute();
+        }
     }
 }
