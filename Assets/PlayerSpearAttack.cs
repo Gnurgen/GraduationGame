d86d1d9c954419a378b87138e3120d92cd546df1
Kristoffer Lind Holm <klh12@student.aau.dk>
2016-11-21 16:28:06 +0100

    spear attack force push,

--- Assets/PlayerSpearAttack.cs
+++ Assets/PlayerSpearAttack.cs
@@ -21,8 +21,10 @@ public class PlayerSpearAttack : MonoBehaviour {
     {
         if (col.tag == "Enemy") // Can bug if the trigger collider enters the same enemy twice
         {
-            col.GetComponent<Health>().decreaseHealth(dmg);
+            col.GetComponent<EnemyStats>().PauseFor(0.2f);
+            col.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Force);
             GameManager.events.PlayerAttackHit(GameManager.player, col.gameObject, dmg);
+            col.GetComponent<Health>().decreaseHealth(dmg);
         }
     }
 }
