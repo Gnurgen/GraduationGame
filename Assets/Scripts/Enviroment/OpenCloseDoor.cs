b5e0dbf638599af3cf397112e4737d8dab6bfa4f
Mette <mette9jakobsen@homail.com>
2016-11-22 14:28:52 +0100

    Fixed vibrations?

---
+++ Assets/Scripts/Enviroment/OpenCloseDoor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenCloseDoor : MonoBehaviour {
+    private GameObject[] listOfDoors;
+
+    void Start () {
+	
+	}
+	
+    void Update () {
+	
+	}
+
+    void openDoors() {
+
+    }
+}
