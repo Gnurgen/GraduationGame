ada97c63e8a9593e0a445f9f2983de15396aa234
Anders Nørgaard <anders.n89@gmail.com>
2016-11-24 10:42:34 +0100

    Added awesome shader pack!

---
+++ Assets/MobileShaderPack/NextGen/SpaceDistortion/Scripts/UVTiling.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UVTiling : MonoBehaviour
+{	
+	public float tilingSpeed = 1.0f;
+
+	void Start ()
+	{
+	}
+
+	void Update () 
+	{
+		Vector2 offset = GetComponent<Renderer>().material.GetTextureOffset("_MainTex");
+		offset.y += tilingSpeed * Time.deltaTime;
+		while(offset.y > 1.0f) offset.y -= 1.0f;
+	
+		GetComponent<Renderer>().material.SetTextureOffset("_MainTex", offset);
+	}
+}
---
+++ Assets/MobileShaderPack/RenderingMask/RenderingOrderSetter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Rendering order setter.
+/// If you have problem with rendering a geometry in front of Mask Object, use this script to set geometry rendering order bigger like "3004".
+/// </summary>
+public class RenderingOrderSetter : MonoBehaviour 
+{
+	[SerializeField] private int order = 3004;
+
+	void Awake () 
+	{
+		Renderer rd = gameObject.GetComponent<Renderer>();
+		if(rd != null)
+		{
+			rd.material.renderQueue = order;
+		}
+	}
+}
