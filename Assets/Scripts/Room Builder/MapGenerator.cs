f6eef14766dd24786a752a5e3337da80d1e1d8dc
Mathias Utne <Muksvendsen@gmail.com>
2016-11-24 13:23:10 +0100

    kris

--- Assets/Scripts/Room Builder/MapGenerator.cs
+++ Assets/Scripts/Room Builder/MapGenerator.cs
@@ -232,7 +232,8 @@ public class MapGenerator : MonoBehaviour {
 
                                 if (indexList.Count > 0)
                                 {
-                                    mask[indexList[Random.Range(0, indexList.Count - 1)]] = offset + 2;
+                                    if (Random.Range(0, 2) < 2)
+                                        mask[indexList[Random.Range(0, indexList.Count - 1)]] = offset + 2;
 
                                     for (i = 0; i < indexList.Count; i++)
                                     {
