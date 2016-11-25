f987df61ae2fcda296a2114efd62c47deb673dff
Kristoffer Lind Holm <klh12@student.aau.dk>
2016-11-25 16:37:43 +0100

    skip2boss yes yes YES

---
+++ Assets/Skip2Boss.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Skip2Boss : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.touchCount > 8 || Input.GetKeyDown(KeyCode.F10))
+        {
+            SceneManager.LoadScene("BossLevel");
+        }
+	}
+}
