d792bf33d961a79323c2ae9d9b38c7141f8b8c5d
Daniel Svejstrup Christensen <fusion_link@hotmail.com>
2016-11-28 09:01:12 +0100

    no message

---
+++ Assets/Scripts/Enviroment/DoorTriggers.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTriggers : MonoBehaviour {
+    public bool isOne = false;
+	void Start () {
+        if (gameObject.name == "trigger1")
+            isOne = true;
+
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+
+        }
+        Debug.Log("Close Doors");
+    }
+}
