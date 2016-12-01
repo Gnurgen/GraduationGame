12a75e1bd6aaf6f43872696a6a63423c80de9daf
Mette <mette9jakobsen@homail.com>
2016-12-01 10:45:24 +0100

    Camera shake

--- Assets/Scripts/CAMERA_MoveWithPlayer.cs
+++ Assets/Scripts/CAMERA_MoveWithPlayer.cs
@@ -8,8 +8,17 @@ public class CAMERA_MoveWithPlayer : MonoBehaviour {
     [SerializeField]
     private Vector3 viewingAngle;
     private Transform player;
-	// Use this for initialization
-	void Awake () {
+
+    //Camera Shakes
+    float duration = 3;
+    float magnitude = 0.2f;
+    bool shaking = false;
+    Vector3 shakingVec;
+    void Start() {
+        GameManager.events.OnBossDeath += shakeCamera;
+    }
+    
+    void Awake () {
         transform.rotation = Quaternion.identity;
         player = GameManager.player.transform;
         transform.Rotate(viewingAngle);
@@ -17,10 +26,28 @@ public class CAMERA_MoveWithPlayer : MonoBehaviour {
             transform.position = player.position - transform.forward * distance;
     }
 	
-	// Update is called once per frame
 	void LateUpdate () {
-        if(player!=null)
+        if (player != null && shaking == false)
+        {
             transform.position = player.position - transform.forward * distance;
+            Debug.Log("Not shaking");
+        }
+        else if (player != null && shaking == true)
+        {
+            Debug.Log("Shaking");
+            shakingVec = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.4f, 0.4f), 0);
+            transform.position = (player.position - transform.forward * distance) + shakingVec;
+        }
+    }
+    public void shakeCamera(GameObject ID)
+    {
+        StartCoroutine(Shake());
+    }
 
+    IEnumerator Shake()
+    {
+        shaking = true;
+        yield return new WaitForSeconds(3);
+        shaking = false;
     }
 }
