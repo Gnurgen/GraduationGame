19ebec7a56c9a85ce2901d73a936712c940066ed
Kenneth Jürgensen <gnurgen@gmail.com>
2016-12-01 14:06:16 +0100

    Spiced up flying spear.

--- Assets/Scripts/Abilities/FlyingSpear.cs
+++ Assets/Scripts/Abilities/FlyingSpear.cs
@@ -13,13 +13,15 @@ public class FlyingSpear : MonoBehaviour {
     private int dragForce = 1;
     [SerializeField]
     private int dragTargets;
+    [SerializeField]
+    private bool multipleHit;
 
 
 
     private float currentCooldown;
     private InputManager IM;
     private EventManager EM;
-    private CurveDraw LR;
+    private EffectCurveDraw LR;
     private float currentDrawLength;
     private List<Vector3> drawnPoints;
     private int ID;
@@ -33,7 +35,7 @@ public class FlyingSpear : MonoBehaviour {
         currentDrawLength = 0;
         IM = GameManager.input;
         EM = GameManager.events;
-        LR = GetComponent<CurveDraw>();
+        LR = GetComponent<EffectCurveDraw>();
         ID = IM.GetID();
 	}
 	
@@ -75,10 +77,12 @@ public class FlyingSpear : MonoBehaviour {
 
         GameObject s = Instantiate(spear) as GameObject;
         GameManager.events.SpearDrawAbilityUsed(s);
-        s.GetComponent<SpearController>().SetParameters(LR.GetPoints(), flyingSpeed, damage, pushForce,dragForce, spearAltitude, turnRate, stunTime, dragTargets);
+        s.GetComponent<SpearControl>().SetParameters(LR.GetPoints(), LR.GetEffects(), flyingSpeed, damage, pushForce,dragForce, spearAltitude, turnRate, stunTime, dragTargets, multipleHit);
         LR.CleanUp();
         currentCooldown = cooldown;
     }
+
+
 
     IEnumerator DrawLine()
     {
