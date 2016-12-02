9e73e0ee898b38299c0408440bc22ef5592457c3
Mette <mette9jakobsen@homail.com>
2016-12-02 08:47:23 +0100

    backup Save

---
+++ Assets/Scripts/Boss/BossCamMoment.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossCamMoment : MonoBehaviour {
+    private BossAI boss;
+    private Vector3 bossT;
+    public GameObject bossTop, bossButtom, bossObj;
+    private CAMERA_MoveWithPlayer cam;
+    private Quaternion headA, headB;
+
+    private float maxAngle, t;
+    private float currentTime = 0;
+    void Start()
+    {
+        bossObj = GameObject.Find("Boss");
+        bossT = GameObject.Find("Boss").transform.position+ new Vector3(0,3,0);
+        boss = GameObject.Find("Boss").GetComponent<BossAI>();
+        cam = Camera.main.GetComponent<CAMERA_MoveWithPlayer>();
+    }
+    void Update()
+    {
+
+   }
+    void OnTriggerEnter(Collider col) 
+    {
+        if (col.tag == "Player") {
+            StartCoroutine(waitForMove());
+            cam.releaseControl();
+        }
+    }
+    IEnumerator turnHead(float angle, float duration, GameObject head) {
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            t = 1 - ((duration - currentTime) / duration);
+            head.transform.localRotation = Quaternion.Euler(0, angle*t,0);
+            yield return null;
+        }
+            
+        currentTime = 0;
+        yield break;
+    }
+    IEnumerator waitForMove() {
+        cam.lookAtObj(GameManager.player.transform.position, bossT,2);
+        yield return new WaitForSeconds(2);
+        yield return StartCoroutine(turnHead(180,2, bossTop));
+        yield return new WaitForSeconds(1);
+        cam.bossShakeCamera(1);
+        yield return StartCoroutine(turnHead(30, 1, bossButtom));
+        boss.Activate();
+        yield return new WaitForSeconds(2);
+        cam.lookAtObj(bossT,GameManager.player.transform.position, 2);
+    }
+}
--- Assets/Scripts/CAMERA_MoveWithPlayer.cs
+++ Assets/Scripts/CAMERA_MoveWithPlayer.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CAMERA_MoveWithPlayer : MonoBehaviour {
+public class CAMERA_MoveWithPlayer : MonoBehaviour
+{
 
     [SerializeField]
     private float distance;
@@ -14,40 +15,77 @@ public class CAMERA_MoveWithPlayer : MonoBehaviour {
     float magnitude = 0.2f;
     bool shaking = false;
     Vector3 shakingVec;
-    void Start() {
+
+    //Force movement for "Cutscenes"
+    bool takeControl = false;
+    float currentT, t;
+
+
+    void Start()
+    {
         GameManager.events.OnBossDeath += shakeCamera;
     }
-    
-    void Awake () {
+
+    void Awake()
+    {
         transform.rotation = Quaternion.identity;
         player = GameManager.player.transform;
         transform.Rotate(viewingAngle);
         if (player != null)
             transform.position = player.position - transform.forward * distance;
     }
-	
-	void LateUpdate () {
-        if (player != null && shaking == false)
+
+    void LateUpdate()
+    {
+        if (player != null &&  !takeControl)
         {
+            Debug.Log("Updating");
             transform.position = player.position - transform.forward * distance;
-            Debug.Log("Not shaking");
-        }
-        else if (player != null && shaking == true)
-        {
-            Debug.Log("Shaking");
-            shakingVec = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.4f, 0.4f), 0);
-            transform.position = (player.position - transform.forward * distance) + shakingVec;
-        }
+        }       
     }
     public void shakeCamera(GameObject ID)
     {
-        StartCoroutine(Shake());
+        StartCoroutine(cameraShake(3));
+    }
+    public void bossShakeCamera(float duration) {
+        StartCoroutine(cameraShake(duration));
     }
 
-    IEnumerator Shake()
+    IEnumerator cameraShake(float duration)
     {
-        shaking = true;
-        yield return new WaitForSeconds(3);
-        shaking = false;
+        Vector3 oriPos = transform.position;
+        while (currentT < duration)
+        {
+            takeControl = true;
+            currentT += Time.deltaTime;
+            shakingVec = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.4f, 0.4f), 0);
+            transform.position = oriPos + shakingVec;
+            yield return null;
+        }
+        transform.position = oriPos;
+        currentT = 0;
+        yield break;
+    }
+
+    public void lookAtObj(Vector3 startPos, Vector3 endPos, float duration)
+    {
+        StartCoroutine(lookatWhatever(startPos, endPos, duration));
+    }
+
+    IEnumerator lookatWhatever(Vector3 startPos, Vector3 endPos, float duration)
+    {
+        while (currentT < duration)
+        {
+            takeControl = true;
+            currentT += Time.deltaTime;
+            t = 1 - ((duration - currentT) / duration);            
+            transform.position = Vector3.Lerp(startPos - transform.forward * distance, endPos - transform.forward * distance, t);
+            yield return null;
+        }
+        currentT = 0;
+        yield break;
+    }
+    public void  releaseControl() {
+        takeControl = false;
     }
 }
