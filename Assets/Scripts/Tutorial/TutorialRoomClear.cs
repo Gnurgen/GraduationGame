5e9be2eb508cc65038abd352a99b58aefe9ccb6e
Mette <mette9jakobsen@homail.com>
2016-12-02 11:34:43 +0100

    Tutorial Programming Fixes

--- Assets/Scripts/Tutorial/TutorialRoomClear.cs
+++ Assets/Scripts/Tutorial/TutorialRoomClear.cs
@@ -2,22 +2,20 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class TriggerBox : MonoBehaviour {
+public class TutorialRoomClear : MonoBehaviour {
     public int nr;
     public Sprite tutorialPic;
     public Image mainCanvas;
-    TutorialClose tut;
 
-    void OnTriggerEnter(Collider col)
-    {      
-        if (col.tag == "Player")
+   
+	void Update () {
+        Health[] enemies = gameObject.transform.parent.gameObject.GetComponentsInChildren<Health>();
+        if (enemies.Length == 0)
         {
-            mainCanvas.gameObject.SetActive(true);  
+            mainCanvas.gameObject.SetActive(true);
             mainCanvas.overrideSprite = tutorialPic;
             GameObject.Find("TutorialText").GetComponent<Text>().text = GameObject.Find("Door").GetComponent<TutorialTexts>().getTextSnipped(nr);
             gameObject.SetActive(false);
         }
     }
-
-
 }
