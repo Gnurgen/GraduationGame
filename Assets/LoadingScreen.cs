8b5bf0dc16dcfd514147647e47e17f50086a8e81
Kristoffer Lind Holm <klh12@student.aau.dk>
2016-12-05 18:16:06 +0100

    more loading screen

--- Assets/LoadingScreen.cs
+++ Assets/LoadingScreen.cs
@@ -7,24 +7,34 @@ using System;
 public class LoadingScreen : MonoBehaviour {
 
     string LoadToScene;
+    private int IMID = 0;
     public Image loadingProgress;
     bool loadComplete = false, subscribed = false, takeControl = false;
-    float currentProgress;
     public bool MapGenerated = false;
     float mapProgress;
     float totalprogress = 1f; // REMEMBER TO CHANGE THIS FOR EVERY PROGRESS SEND
+
+    // animation variables
+    public Text LoadingText;
+    public Image ImgAni;
+    public Sprite[] ShuffleAnimation = new Sprite[8];
+    private int saFrame = 0; 
 	// Use this for initialization
 	void Start () {
         loadingProgress = GameObject.Find("LoadProgress").GetComponent<Image>();
         loadingProgress.fillAmount = 0f;
-
+        LoadingText.text = "L O A D I N G . . .";
         if (GameManager.progress == 0)
         {
+            ImgAni.enabled = false;
             LoadToScene = "Tutorial"; 
             StartCoroutine(tutLevel());
         }
         else if (GameManager.progress <= GameManager.numberOfLevels) // Number of levels before Boss level 
         {
+            saFrame = 0;
+            ImgAni.enabled = true;
+            ImgAni.sprite = ShuffleAnimation[saFrame];
             LoadToScene = "Final";
             SceneManager.LoadSceneAsync("Final", LoadSceneMode.Additive);
            
@@ -32,6 +42,7 @@ public class LoadingScreen : MonoBehaviour {
         }
         else  // Boss Level
         {
+            ImgAni.enabled = false;
             LoadToScene = "BossLevel";
             StartCoroutine(tutLevel());
         }
@@ -44,13 +55,13 @@ public class LoadingScreen : MonoBehaviour {
         if(GameManager.events != null && !subscribed)
         {
             GameManager.events.OnLoadingProgress += Loading;
-            GameManager.events.OnLoadComplete += UnloadLoadingScene;
             GameManager.events.OnMapGenerated += ExtraSecurity;
             subscribed = true;
         }
         if(GameManager.input != null && !takeControl)
         {
-            GameManager.input.TakeControl(gameObject.GetInstanceID());
+            IMID = GameManager.input.GetID(); 
+            GameManager.input.TakeControl(IMID);
             takeControl = true;
         }
     }
@@ -62,24 +73,51 @@ public class LoadingScreen : MonoBehaviour {
 
     private void Loading(float loadingprogress) // FOR FINAL SCENE
     {
+       
         if (loadingprogress <= 1f) // Daniels MAPPROGRESS SAVED
                 mapProgress = loadingprogress;
 
-                loadingProgress.fillAmount = (currentProgress + mapProgress) / totalprogress;
-            if (currentProgress + mapProgress >= totalprogress && !loadComplete && MapGenerated)
-            {
-                loadComplete = true;
-            SceneManager.SetActiveScene(SceneManager.GetSceneByName(LoadToScene));
-            GameManager.events.LoadComplete();
-            }
+        loadingProgress.fillAmount = mapProgress / totalprogress;
+
+        if (mapProgress >= totalprogress && !loadComplete && MapGenerated)
+        {
+            loadComplete = true;
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(LoadToScene));
+        StartCoroutine(waitingForInput(LoadToScene == "Final"));
+        }
+
+        saFrame++;
+        if (saFrame >= ShuffleAnimation.Length)
+            saFrame = 0;
+        ImgAni.sprite = ShuffleAnimation[saFrame];
+
     }
-    private void UnloadLoadingScene()
+    
+    private IEnumerator waitingForInput(bool animation)
     {
-        GameManager.input.ReleaseControl(gameObject.GetInstanceID());
+        print("ani: " + animation);
+        LoadingText.text = "T O U C H   T O   C O N T I N U E";
+        while (Input.touchCount < 1 && !Input.GetKeyDown(KeyCode.Space))
+        {
+            if (animation)
+            {
+                if (saFrame >= ShuffleAnimation.Length - 1)
+                    saFrame = 0;
+                else
+                    saFrame++;
+                ImgAni.sprite = ShuffleAnimation[saFrame];
+                yield return new WaitForSeconds(0.0625f);
+            }
+            yield return null;
+        }
+      
+        GameManager.events.LoadComplete();
+        GameManager.input.ReleaseControl(IMID);
         GameManager.events.FadeFromBlackToTransparent();
         SceneManager.MergeScenes(SceneManager.GetSceneByName("LoadingScreen"), SceneManager.GetSceneByName(LoadToScene));
         Destroy(GameObject.Find("LoadingCamera"));
         Destroy(gameObject);
+        yield break;
     }
 
     private IEnumerator tutLevel() //Tutorial level loading
@@ -96,7 +134,7 @@ public class LoadingScreen : MonoBehaviour {
             }
             yield return null;
         }
-        GameManager.events.LoadComplete();
+        StartCoroutine(waitingForInput(LoadToScene == "Final"));
         yield return null;
     }
 }
